4ea54f8750597bf501933b77cfc3a733779c675e
kchow3 <kchow3@ualberta.ca>
2014-01-14 09:07:35 -0700

    Fixed counter class

---
+++ src/com/example/as1/counter.java
@@ -0,0 +1,16 @@
+package com.example.as1;
+import java.util.*;
+
+public class counter {
+	
+	private String name;
+	private int count;
+	private Date time[];
+	
+	//Constructor method
+	public counter()
+	{
+		
+	}
+
+}
