7e5c015ccb637a60a979d238801e009f7b855b02
kchow3 <kchow3@ualberta.ca>
2014-01-15 09:14:49 -0700

    Added methods to counter class.

--- src/com/example/as1/counter.java
+++ src/com/example/as1/counter.java
@@ -1,16 +1,50 @@
 package com.example.as1;
 import java.util.*;
+import java.
 
 public class counter {
 	
 	private String name;
 	private int count;
-	private Date time[];
+	private Arraylist<Date> time;
 	
 	//Constructor method
-	public counter()
+	public counter(String counter_name)
 	{
-		
+		this.name = counter_name;
+		this.count = 0;
+		this.time = new Date[]();
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public int getCount()
+	{
+		return count;
+	}
+	
+	public Date[] getDate()
+	{
+		return time;
+	}
+	
+	public void increase()
+	{
+		count++;
+	}
+	
+	public void change_name(String new_name)
+	{
+		this.name = new_name;
+	}
+	
+	public void reset_counter()
+	{
+		this.count = 0;
+		this.time = new
 	}
 
 }
