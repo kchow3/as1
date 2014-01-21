aa41a604c2d5a169553165fdcb136ea967e0768f
kchow3 <kchow3@ualberta.ca>
2014-01-20 20:48:17 -0700

    Starting to add the onclick listners for the custom listview, still need to fix connecting custom lsitview row to the controller.

--- src/es/softwareprocess/as1/ListviewAdapter.java
+++ src/es/softwareprocess/as1/ListviewAdapter.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
@@ -13,6 +14,7 @@ import android.widget.Button;
 public class ListviewAdapter extends ArrayAdapter<Counter>{
 
 	private final Context context;
+	private ViewHolder holder;
 	private final ArrayList<Counter> countersArrayList;
 	
 	public ListviewAdapter(Context context, ArrayList<Counter> countersArrayList)
@@ -22,10 +24,18 @@ public class ListviewAdapter extends ArrayAdapter<Counter>{
 		this.context = context;
 		this.countersArrayList = countersArrayList;
 	}
+	 
+	private static class ViewHolder 
+	{
+         TextView nameView, countView;
+         Button incrementButton, resetButton, deleteButton;
+    }
 	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent)
 	{
+		holder = new ViewHolder();
+		
 		//create inflater
 		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		
@@ -34,18 +44,45 @@ public class ListviewAdapter extends ArrayAdapter<Counter>{
 		rowView = inflater.inflate(R.layout.listview_row, parent, false);
 		
 		//get name, counter and buttons views from the rowView
-		TextView nameView = (TextView) rowView.findViewById(R.id.nameView);
-		TextView countView = (TextView) rowView.findViewById(R.id.countView);
-		Button incrementButton = (Button) rowView.findViewById(R.id.incrementButton);
-		Button resetButton = (Button) rowView.findViewById(R.id.resetButton);
-		Button deleteButton = (Button) rowView.findViewById(R.id.deleteButton);
+		holder.nameView = (TextView) rowView.findViewById(R.id.nameView);
+		holder.countView = (TextView) rowView.findViewById(R.id.countView);
+		holder.incrementButton = (Button) rowView.findViewById(R.id.incrementButton);
+		holder.resetButton = (Button) rowView.findViewById(R.id.resetButton);
+		holder.deleteButton = (Button) rowView.findViewById(R.id.deleteButton);
 		
 		//set text for textview
-		nameView.setText(countersArrayList.get(position).getName());
-		countView.setText(Integer.toString(countersArrayList.get(position).getCount()));
+		holder.nameView.setText(countersArrayList.get(position).getName());
+		holder.countView.setText(Integer.toString(countersArrayList.get(position).getCount()));
 		//TODO create onclick listners for button clicks
+		
+		holder.incrementButton.setOnClickListener(incrementBtnClick);
+        holder.incrementButton.setTag(position);
 		
 		return rowView;
 	}
+	
+	@Override
+    public Counter getItem(int position) {
+        // TODO Auto-generated method stub
+        return countersArrayList.get(position);
+    }
+	
+	private OnClickListener incrementBtnClick = new OnClickListener() 
+	{
+	    public void onClick(View v)
+	    {
+	    	
+	    }
+	};
+    
+    public void deleteClick(View v)
+    {
+    	
+    }
+    
+    public void resetClick(View v)
+    {
+    	
+    }
 	
 }
