64e61c39ee45fdf31ddb8debbfa8427a2173076e
kchow3 <kchow3@ualberta.ca>
2014-01-28 08:55:25 -0700

    Added code to the stats activity so that button for counters closes the actvity and code for getting the intent that contains the list of stats from the main activity.

--- src/es/softwareprocess/as1/StatsActivity.java
+++ src/es/softwareprocess/as1/StatsActivity.java
@@ -1,52 +1,43 @@
 package es.softwareprocess.as1;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.support.v4.app.NavUtils;
 
 public class StatsActivity extends Activity {
+	
+	private ArrayList<String> stats;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+	protected void onCreate(Bundle icicle) 
+	{
+		super.onCreate(icicle);
 		setContentView(R.layout.activity_stats);
-		// Show the Up button in the action bar.
-		setupActionBar();
+		getInputList();
 	}
-
-	/**
-	 * Set up the {@link android.app.ActionBar}.
-	 */
-	private void setupActionBar() {
-
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-
+	
+	protected void onResume()
+	{
+		super.onResume();
+		getInputList();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.stats, menu);
-		return true;
+	
+	public void countersBtnClick(View v)
+	{
+	    finish(); 
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	
+	private void getInputList()
+	{
+		Intent in = getIntent();
+		ArrayList<String> list =(ArrayList<String>) in.getSerializableExtra(MainActivity.EXTRA_STATS);
+		stats.addAll(list);
 	}
 
 }
